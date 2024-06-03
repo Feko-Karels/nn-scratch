757820667f1d6760332bd7a03cd8bde542777cd1
Feko-Karels <feko.karels@gmx.de>
2024-06-03 15:00:32 +0900

    Change structure to use model class
    Signed-off-by: Feko-Karels <feko.karels@gmx.de>

--- nn.py
+++ nn.py
@@ -8,14 +8,16 @@ m, n = train_data.shape
 
 class NN():
     def __init__(self):
-        self.theta = self.init_params()
-
-    def init_params(self):
         W1 = np.random.rand(10, 784) - 0.5
         b1 = np.random.rand(10, 1) - 0.5
         W2 = np.random.rand(10, 10) - 0.5
         b2 = np.random.rand(10, 1) - 0.5
-        return W1, b1, W2, b2
+        self.theta = W1, b1, W2, b2
+        self.Z1 = np.zeros((10,1))
+        self.A1 = np.zeros((10,1))
+        self.Z2 = np.zeros((10,1))
+        self.A2 = np.zeros((10,1))
+        self.grads = None
 
     def ReLU(self, Z):
         return np.maximum(Z, 0)
@@ -24,77 +26,76 @@ class NN():
         A = np.exp(Z) / sum(np.exp(Z))
         return A
         
-    def forward_prop(self, theta, X):
-        W1, b1, W2, b2 = theta
-        Z1 = W1.dot(X) + b1
-        A1 = self.ReLU(Z1)
-        Z2 = W2.dot(A1) + b2
-        A2 = self.softmax(Z2)
-        return Z1, A1, Z2, A2
+    def forward(self, X):
+        W1, b1, W2, b2 = self.theta
+        self.Z1 = W1.dot(X) + b1
+        self.A1 = self.ReLU(self.Z1)
+        self.Z2 = W2.dot(self.A1) + b2
+        self.A2 = self.softmax(self.Z2)
+        return self.A2
 
     def ReLU_deriv(self, Z):
         return Z > 0
 
     def one_hot(self, Y):
-        one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-        one_hot_Y[np.arange(Y.size), Y] = 1
-        one_hot_Y = one_hot_Y.T
+        one_hot_Y = np.zeros((10, Y.size))  # Ensure one_hot_Y has 10 rows for 10 classes
+        one_hot_Y[Y, np.arange(Y.size)] = 1
         return one_hot_Y
 
-    def backward_prop(self, Z1, A1, Z2, A2, W1, W2, X, Y):
+    def backward_prop(self, X, Y):
+        _, _, W2, _ = self.theta
         one_hot_Y = self.one_hot(Y)
-        dZ2 = A2 - one_hot_Y
-        dW2 = 1 / m * dZ2.dot(A1.T)
+        dZ2 = self.A2 - one_hot_Y
+        dW2 = 1 / m * dZ2.dot(self.A1.T)
         db2 = 1 / m * np.sum(dZ2)
-        dZ1 = W2.T.dot(dZ2) * self.ReLU_deriv(Z1)
+        dZ1 = W2.T.dot(dZ2) * self.ReLU_deriv(self.Z1)
         dW1 = 1 / m * dZ1.dot(X.T)
         db1 = 1 / m * np.sum(dZ1)
-        return dW1, db1, dW2, db2
+        self.grads = dW1, db1, dW2, db2
 
-    def update_params(self, theta, dW1, db1, dW2, db2, alpha):
-        W1, b1, W2, b2 = theta
+    def basic_sdg(self, alpha):
+        W1, b1, W2, b2 = self.theta
+        dW1, db1, dW2, db2 = self.grads
         W1 = W1 - alpha * dW1
         b1 = b1 - alpha * db1    
         W2 = W2 - alpha * dW2  
         b2 = b2 - alpha * db2    
-        return W1, b1, W2, b2
+        self.theta = W1, b1, W2, b2
     
     def cross_entropy_loss(self, A2, Y):
         m = Y.shape[0]
-        loss = np.sum(-np.log(A2[Y, range(m)])) / m
+        loss = np.sum(-np.log(A2[Y, np.arange(m)])) / m
         return loss
     
     def get_predictions(self, X):
-        _, _, _, out = self.forward_prop(self.theta, X)
+        out = self.forward(X)
         return np.argmax(out, 0)
 
     def get_accuracy(self, predictions, Y):
         return np.sum(predictions == Y) / Y.size
     
-    def sgd(self, data, batch_size=100, alpha=0.15, epochs=500):
-
-        for epoch in range(epochs):
-            np.random.shuffle(data)
-            train_data = data[:].T
-            Y = train_data[0]
-            X = train_data[1:n]
-            X = X / 255.
-
-            loss = 0
-            for i in range(0, X.shape[1], batch_size):
-                X_batch = X[:,i:i+batch_size]
-                Y_batch = Y[i:i+batch_size]
-                Z1, A1, Z2, A2 = self.forward_prop(self.theta, X_batch)
-                dW1, db1, dW2, db2 = self.backward_prop(Z1, A1, Z2, A2, self.theta[0], self.theta[2], X_batch, Y_batch)
-                self.theta = self.update_params(self.theta, dW1, db1, dW2, db2, alpha)
-                loss += self.cross_entropy_loss(A2, Y_batch)
-            if epoch%10 == 0:
-                print("epoch: ", epoch)
-                predictions = self.get_predictions(X)
-                print('Average loss is: ', loss/batch_size)
-                print('Accuracy is: ', self.get_accuracy(predictions, Y))
-        return self.theta
-
-#W1, b1, W2, b2 = sdg(X_train,Y_train)
+batch_size=100
+alpha=0.15
+epochs=500
 model = NN()
-theta = model.sgd(train_data)
+
+for epoch in range(epochs):
+    np.random.shuffle(train_data)
+    train_data = train_data[:].T
+    Y = train_data[0]
+    X = train_data[1:n]
+    X = X / 255.
+
+    loss = 0
+    for i in range(0, X.shape[1], batch_size):
+        X_batch = X[:,i:i+batch_size]
+        Y_batch = Y[i:i+batch_size]
+        out = model.forward(X_batch)
+        model.backward_prop(X_batch, Y_batch)
+        model.basic_sdg(alpha)
+        loss += model.cross_entropy_loss(out, Y_batch)
+    if epoch%10 == 0:
+        print("epoch: ", epoch)
+        predictions = model.get_predictions(X)
+        print('Average loss is: ', loss/batch_size)
+        print('Accuracy is: ', model.get_accuracy(predictions, Y))
